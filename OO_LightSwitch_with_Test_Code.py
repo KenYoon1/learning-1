e4faad3d49148f3addf5b587df7d57822c34106f
fenmir <fenmir@gmail.com>
2024-01-21 16:34:55 +0900

    chap2

--- OO_LightSwitch_with_Test_Code.py
+++ OO_LightSwitch_with_Test_Code.py
@@ -1,15 +1,18 @@
 # OO_LightSwitch
 class LightSwitch():
- def __init__(self):
- self.switchIsOn = False
- def turnOn(self):
- # turn the switch on 
- self.switchIsOn = True
- def turnOff(self):
- # turn the switch off
- self.switchIsOn = False
- def show(self): # added for testing
- print(self.switchIsOn)
+    def __init__(self):
+        self.switchIsOn = False 
+
+    def turnOn(self):
+    # turn the switch on 
+        self.switchIsOn = True
+
+    def turnOff(self):
+    # turn the switch off
+        self.switchIsOn = False
+
+    def show(self): # added for testing
+        print(self.switchIsOn)
  
 # Main code
 oLightSwitch = LightSwitch() # create a LightSwitch object
