77ca9ab87adf6a9da5d1b9176780c58d1ea1fcaf
Ken-home-pc <fenmir@gmail.com>
2024-01-22 01:24:18 +0900

    chap3

--- Chap2/TV.py
+++ Chap2/TV.py
@@ -1,7 +1,11 @@
 # TV class
 
 class TV():
-    def __init__(self):
+    def __init__(self, brand, location):
+
+        self.brand = brand
+        self.location = location 
+
         self.isOn = False
         self.isMuted = False
 
@@ -56,7 +60,8 @@ class TV():
     
     def showInfo(self):
         print()
-        print('TV Status:')
+        print('Status of TV:', self.brand)
+        print('     Location:', )
         if self.isOn:
             print('     TV is: on')
             print('     Channel is:', self.channelList[self.channelIndex])
@@ -67,31 +72,6 @@ class TV():
         else:
             print('     TV is: Off')
 
-# Main code
-oTV = TV() # create the TV object
-# Turn the TV on and show the status
-oTV.power()
-oTV.showInfo()
-# Change the channel up twice, raise the volume twice, show status
-oTV.channelUp()
-oTV.channelUp()
-oTV.volumeUp()
-oTV.volumeUp()
-oTV.showInfo()
-# Turn the TV off, show status, turn the TV on, show status
-oTV.power()
-oTV.showInfo()
-oTV.power()
-oTV.showInfo()
-# Lower the volume, mute the sound, show status
-oTV.volumeDown()
-oTV.mute()
-oTV.showInfo()
-# Change the channel to 11, mute the sound, show status
-oTV.setChannel(11)
-oTV.mute()
-oTV.showInfo()
+ 
 
-test!!
-        
 
